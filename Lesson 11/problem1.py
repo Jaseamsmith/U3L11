cdf551e14bea01ff793ae43e96aa83f6cd7c96be
Jaseamsmith <43823302+Jaseamsmith@users.noreply.github.com>
2018-12-19 12:40:21 -0500

    Add files via upload

---
+++ Lesson 11/problem1.py
@@ -0,0 +1,9 @@
+print ('-'*60)
+print ('Iz iT Brick Bot?')
+print ()
+answer = input ('Temaperatur in Fahrenheit')
+if  weather <= '32':
+	print ('Its freezing outside wear a coat !!')
+else:
+	Print ('Its not freaking hot wear a coat !!')
+	print ('-'*60)
---
+++ Lesson 11/problem2.py
@@ -0,0 +1,9 @@
+print ('-'*60)
+print ('Narrative Bot:')
+print ()
+Name = input ('Cody')
+if Name <= 'Grade: 65:'
+	print ('Narrative : Cody, your final grade in AP Computer Science is 62% ! You have failed most components of the class! I ask you to work harder in next semester of AP Computer Science!')
+else:
+	Print ('Congrats you have done an amazing job this year! Keep up the good work and lets continue to work hard next semester!')
+	print ('-'*60)
